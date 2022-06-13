2858c68b13b73fd4bf4d63bf805a2c6884e69529
ALEH FIADOSAU <33495591+Solinx97@users.noreply.github.com>
2022-06-13 19:31:18 +0300

    Create Data Access Layer
    Using patern(s): Unit of Work

---
+++ src/CombatAnalysis.DAL/Data/CombatAnalysisContext.cs
@@ -0,0 +1,15 @@
+﻿using CombatAnalysis.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CombatAnalysis.DAL.Data
+{
+    public class CombatAnalysisContext : DbContext
+    {
+        public CombatAnalysisContext(
+            DbContextOptions<CombatAnalysisContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Combat> Combat { get; set; }
+    }
+}
---
+++ src/CombatAnalysis.DAL/Interfaces/IGenericRepositroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CombatAnalysis.DAL.Interfaces
+{
+    public interface IGenericRepository<TModel>
+        where TModel : class
+    {
+        Task<int> CreateAsync(TModel item);
+
+        Task<int> UpdateAsync(TModel item);
+
+        Task<int> DeleteAsync(TModel item);
+
+        Task<TModel> GetByIdAsync(int id);
+
+        Task<IEnumerable<TModel>> GetAllAsync();
+    }
+}
