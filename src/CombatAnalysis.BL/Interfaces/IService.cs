781552c9c055b8278f3df435f70bcd1d08d38176
ALEH FIADOSAU <33495591+Solinx97@users.noreply.github.com>
2022-06-13 19:55:19 +0300

    Create Business Layer

---
+++ src/CombatAnalysis.BL/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CombatAnalysis.BL.Interfaces
+{
+    public interface IService<TModel>
+        where TModel : class
+    {
+        Task<int> CreateAsync(TModel item);
+
+        Task<int> UpdateAsync(TModel item);
+
+        Task<int> DeleteAsync(TModel item);
+
+        Task<IEnumerable<TModel>> GetAllAsync();
+
+        Task<TModel> GetByIdAsync(int id);
+    }
+}
