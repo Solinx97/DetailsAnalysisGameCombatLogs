f3830c066d0698f79d20bebcf2a8e1b486dce4d9
ALEH FIADOSAU <firstkillinbattle@gmail.com>
2022-08-16 16:13:09 +0200

    Add App Center in WPF

--- src/CombatAnalysis.App/App.xaml.cs
+++ src/CombatAnalysis.App/App.xaml.cs
@@ -1,11 +1,22 @@
-﻿using MvvmCross.Core;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using MvvmCross.Core;
 using MvvmCross.Platforms.Wpf.Core;
 using MvvmCross.Platforms.Wpf.Views;
+using System.Windows;
 
 namespace CombatAnalysis.App
 {
     public partial class App : MvxApplication
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppCenter.Start("1752e65c-dec2-4f55-971c-28ef864a77c1",
+                typeof(Analytics), typeof(Crashes));
+        }
+
         protected override void RegisterSetup()
         {
             this.RegisterSetupType<MvxWpfSetup<Core.App>>();
