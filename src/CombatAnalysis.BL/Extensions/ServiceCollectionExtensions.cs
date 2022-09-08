f7beb6c30c30931237aded8c7ac1f5db341158f4
ALEH FIADOSAU <33495591+Solinx97@users.noreply.github.com>
2022-09-08 13:55:16 +0200

    Add new entity CombatLogByUser

--- src/CombatAnalysis.BL/Extensions/ServiceCollectionExtensions.cs
+++ src/CombatAnalysis.BL/Extensions/ServiceCollectionExtensions.cs
@@ -14,6 +14,7 @@ namespace CombatAnalysis.BL.Extensions
             services.RegisterDependenciesDAL(configuration, connectionName);
 
             services.AddScoped<IService<CombatLogDto>, CombatLogService>();
+            services.AddScoped<IService<CombatLogByUserDto>, CombatLogByUserService>();
             services.AddScoped<IService<CombatDto>, CombatService>();
             services.AddScoped<IService<CombatPlayerDataDto>, CombatPlayerService>();
             services.AddScoped<IService<DamageDoneDto>, DamageDoneService>();
