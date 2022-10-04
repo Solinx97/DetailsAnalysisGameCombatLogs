2e0041022aefe0f8339b56c74ac24b84fd3546b2
ALEH FIADOSAU <33495591+Solinx97@users.noreply.github.com>
2022-10-05 00:30:31 +0200

    Update business card

---
+++ businessCard/src/scripts/main.js
@@ -0,0 +1,39 @@
+function appSwitch() {
+    let appSwitcher = document.querySelector("#app-switcher");
+    let desktopApp = document.querySelector(".applications__desktop-application");
+    let webApp = document.querySelector(".applications__web-application");
+
+    appSwitcher.addEventListener("change", (e) => {
+        if (appSwitcher.checked) {
+            webApp.classList.remove("active");
+            desktopApp.classList.add("active");
+        }
+        else {
+            desktopApp.classList.remove("active");
+            webApp.classList.add("active");
+        }
+    });
+}
+
+function projectSwitch() {
+    let openPersonalProject = document.querySelector(".open-personal-project");
+    let closePersonalProject = document.querySelector(".close-personal-project");
+    let personalProject = document.querySelector(".personal-project");
+
+    openPersonalProject.addEventListener("click", () => {
+        openPersonalProject.classList.remove("active");
+        closePersonalProject.classList.add("active");
+
+        personalProject.classList.add("active");
+    });
+
+    closePersonalProject.addEventListener("click", () => {
+        closePersonalProject.classList.remove("active");
+        openPersonalProject.classList.add("active");
+
+        personalProject.classList.remove("active");
+    });
+}
+
+appSwitch();
+projectSwitch();
