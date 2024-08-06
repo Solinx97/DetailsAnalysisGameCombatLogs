e034f32149339d17886b810a986a13ccb4daa50a
OIeg <33495591+Solinx97@users.noreply.github.com>
2024-08-06 11:49:59 +0200

    Update terraform
    Update terraform: add create MS SQL Server and DBs

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,15 @@
 locals {
     env = "dev"
-    region = "eastus2"
+    region = "westus2"
     resource_group_name = "combat-analysis"
     eks_name = "encounters-analysis"
     eks_version = "1.27"
+    sql_server_name = "mysolinxserver"
+    user_db_name = "Combat_Analysis.User"
+    identity_db_name = "Combat_Analysis.Identit"
+    chat_db_name = "Combat_Analysis.Chat"
+    combat_logs_db_name = "Combat_Analysis.Combat_Logs"
+    communication_db_name = "Combat_Analysis.Communication"
+    sql_administrator_login = "oleg123"
+    sql_administrator_login_password = "der45-jui8n-7hjkl-9iuyt"
 }
