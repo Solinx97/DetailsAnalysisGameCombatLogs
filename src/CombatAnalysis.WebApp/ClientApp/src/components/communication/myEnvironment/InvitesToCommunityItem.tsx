9f41b2ac713747aae38bd081bb974e31928589b7
OIeg <firstkillinbattle@gmail.com>
2025-02-15 11:53:12 +0100

    Add Typescript components
    Add Typescript components

--- src/CombatAnalysis.WebApp/ClientApp/src/components/communication/myEnvironment/InvitesToCommunityItem.tsx
+++ src/CombatAnalysis.WebApp/ClientApp/src/components/communication/myEnvironment/InvitesToCommunityItem.tsx
@@ -2,13 +2,14 @@ import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useGetUserByIdQuery } from '../../../store/api/user/Account.api';
-import { useGetCommunityByIdQuery } from '../../../store/api/core/Community.api';
 import { useCreateCommunityUserMutation } from '../../../store/api/community/CommunityUser.api';
 import { useRemoveCommunityInviteMutation } from '../../../store/api/community/InviteToCommunity.api';
+import { useGetCommunityByIdQuery } from '../../../store/api/core/Community.api';
+import { useGetUserByIdQuery } from '../../../store/api/user/Account.api';
+import { InvitesToCommunityItemProps } from '../../../types/components/communication/myEnvironment/InvitesToCommunityItemProps';
 import User from '../User';
 
-const InvitesToCommunityItem = ({ user, inviteToCommunity }) => {
+const InvitesToCommunityItem: React.FC<InvitesToCommunityItemProps>  = ({ user, inviteToCommunity }) => {
     const { t } = useTranslation("communication/myEnvironment/invitesToCommunityItem");
 
     const { data: community, isLoading: communityIsLoading } = useGetCommunityByIdQuery(inviteToCommunity?.communityId);
@@ -47,19 +48,17 @@ const InvitesToCommunityItem = ({ user, inviteToCommunity }) => {
             <div className="request-to-connect__content">
                 <User
                     me={user}
-                    itIsMe={false}
                     targetUserId={inviteOwner.id}
                     setUserInformation={setUserInformation}
-                    allowRemoveFriend={false}
                 />
                 <div>{t("SentInvite")}</div>
                 <div className="community-name">{community.name}</div>
             </div>
             <div className="request-to-connect__answer">
-                <div className="accept"><FontAwesomeIcon icon={faCircleCheck} title={t("Accept")}
-                    onClick={async () => await acceptRequestAsync()} /></div>
-                <div className="reject"><FontAwesomeIcon icon={faCircleXmark} title={t("Reject")}
-                    onClick={async () => await rejectRequestAsync()} /></div>
+                <div className="accept"><FontAwesomeIcon icon={faCircleCheck} title={t("Accept") || ""}
+                    onClick={acceptRequestAsync} /></div>
+                <div className="reject"><FontAwesomeIcon icon={faCircleXmark} title={t("Reject") || ""}
+                    onClick={rejectRequestAsync} /></div>
             </div>
             {userInformation}
         </div>
---
+++ src/CombatAnalysis.WebApp/ClientApp/src/types/components/communication/myEnvironment/InvitesToCommunityItemProps.ts
@@ -0,0 +1,7 @@
+import { AppUser } from "../../../AppUser";
+import { InviteToCommunity } from "../../../InviteToCommunity";
+
+export interface InvitesToCommunityItemProps {
+    user: AppUser;
+    inviteToCommunity: InviteToCommunity;
+}
