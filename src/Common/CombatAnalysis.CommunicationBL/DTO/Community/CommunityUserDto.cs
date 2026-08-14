f7e64784203f5f5997c4e72501d67a66b912a547
Федосов Олег <firstkillinbattle@gmail.com>
2026-08-14 15:22:51 +0300

    Remove unused Communication libraries
    Remove unused Communication libraries:
     - CommunicationDAL
     - CommunicationBL

--- src/Common/CombatAnalysis.CommunicationBL/DTO/Community/CommunityUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CombatAnalysis.CommunicationBL.DTO.Community;
-
-public record CommunityUserDto(
-    string Id,
-    string Username,
-    string AppUserId,
-    int CommunityId
-    );
--- src/Common/CombatAnalysis.CommunicationBL/DTO/Community/InviteToCommunityDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CombatAnalysis.CommunicationBL.DTO.Community;
-
-public record InviteToCommunityDto(
-    int Id,
-    int CommunityId,
-    string ToAppUserId,
-    DateTimeOffset When,
-    string AppUserId
-    );
